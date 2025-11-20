353d7a7c454239788c4de3f26ed9bef21dacc5c5
Domskie08 <sanchezjdominic@gmail.com>
2025-11-20 10:16:07 +0800

    final attendance

--- vl53l0x.py
+++ vl53l0x.py
@@ -1,36 +1,39 @@
-# Minimal VL53L0X driver for Raspberry Pi 5 (no Blinka)
-# Uses smbus2 and direct register access.
-
 import time
 from smbus2 import SMBus
 
-VL53_I2C_ADDR = 0x29
-
 class VL53L0X:
-    def __init__(self, bus=1):
-        self.i2c = SMBus(bus)
-        self.address = VL53_I2C_ADDR
-        self._init_sensor()
+    def __init__(self, address=0x29, bus=1):
+        self.address = address
+        self.bus = SMBus(bus)
 
-    def _write(self, reg, value):
-        self.i2c.write_byte_data(self.address, reg, value)
-
-    def _read16(self, reg):
-        data = self.i2c.read_i2c_block_data(self.address, reg, 2)
-        return (data[0] << 8) | data[1]
-
-    def _init_sensor(self):
-        time.sleep(0.2)   # allow sensor boot
+        # Init sequence for VL53L0X (works on Raspberry Pi 5)
         self._write(0x88, 0x00)
         self._write(0x80, 0x01)
         self._write(0xFF, 0x01)
         self._write(0x00, 0x00)
-        self._write(0x91, 0x3c)
+        self._write(0x91, 0x3C)
         self._write(0x00, 0x01)
         self._write(0xFF, 0x00)
         self._write(0x80, 0x00)
-        time.sleep(0.05)
+
+    def _write(self, reg, value):
+        self.bus.write_byte_data(self.address, reg, value)
+
+    def _read16(self, reg):
+        high = self.bus.read_byte_data(self.address, reg)
+        low = self.bus.read_byte_data(self.address, reg + 1)
+        return (high << 8) | low
 
     @property
     def range(self):
-        return self._read16(0x14)
+        # Start measurement
+        self._write(0x00, 0x01)
+        time.sleep(0.01)
+
+        distance = self._read16(0x14)
+
+        # If invalid, return 0
+        if distance in (0, 0xFFFF):
+            return 0
+
+        return distance
