62bbc0fbb3c0b1f705681958129f027ab627be7b
Domskie08 <sanchezjdominic@gmail.com>
2025-11-20 10:36:29 +0800

    final attendance

--- LED-flash.py
+++ LED-flash.py
@@ -1,7 +1,25 @@
 import time
 import lgpio
 import requests
-from vl53l0x import VL53L0X
+import VL53L0X
+
+class VL53L0X:
+    def __init__(self, address=0x29, bus=1):
+        self.tof = VL53L0X.VL53L0X(i2c_bus=bus, i2c_address=address)
+        self.tof.start_ranging(VL53L0X.VL53L0X_BEST_ACCURACY_MODE)
+
+    def read_range(self):
+        return self.tof.get_distance()
+
+    # Keep old methods for compatibility, but they do nothing
+    def _write(self, reg, value):
+        pass
+
+    def _read(self, reg):
+        return 0
+
+    def _read16(self, reg):
+        return 0
 
 # ------------ CONFIG ----------------
 VL53_THRESHOLD = 1000
