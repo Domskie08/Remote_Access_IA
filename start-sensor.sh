2e53ae5f860bb41857e03cf0d40b5a5c6380b33c
Domskie08 <sanchezjdominic@gmail.com>
2025-11-28 17:43:04 +0800

    Update

---
+++ start-sensor.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+# -----------------------------------------
+# Script to launch VL53L0X sensor app and Chromium kiosk inside venv
+# -----------------------------------------
+
+# Path to your project folder
+PROJECT_DIR="/home/pi/Remote_Access_IA"
+
+# Activate virtual environment inside project folder
+source "$PROJECT_DIR/venv/bin/activate"
+
+# Run the Python app
+python3 "$PROJECT_DIR/LED-flash2.py"
