1dbc9be6fae5a4be1694b453e59048a220908da4
Domskie08 <sanchezjdominic@gmail.com>
2025-12-06 14:51:25 +0800

    Final Update

--- LED-flash2.py
+++ LED-flash2.py
@@ -61,7 +61,6 @@ def start_program():
         "--ignore-ssl-errors-ignore-untrusted",
         "--use-fake-ui-for-media-stream",  # Auto-grant camera/microphone permissions
         "--autoplay-policy=no-user-gesture-required",  # Allow autoplay
-        "--disable-popup-blocking",  # Prevent popups
         WEB_URL
     ])
 
